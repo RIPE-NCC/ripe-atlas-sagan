dc3f64715977e38371f3048b8af711010bf8fe7f
Daniel Quinn <dquinn@ripe.net>
2014-05-19 15:29:57 +0200

    Smarter setup.py to handle different (but compatible) versions of pyOpenSSL

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ install_requires = [
 
 # pyOpenSSL support is flakey on some systems (I'm looking at you Apple)
 if "SAGAN_WITHOUT_SSL" not in os.environ:
-    install_requires.append("pyOpenSSL==0.13")
+    install_requires.append("pyOpenSSL>=0.12")
+    install_requires.append("pyOpenSSL<0.14")
 
 # Like pyOpenSSL, dnspython might be problematic for some
 if "SAGAN_WITHOUT_DNS" not in os.environ:
@@ -24,6 +25,11 @@ if "SAGAN_WITHOUT_DNS" not in os.environ:
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+# Get the long description from README.md
+long_description = None
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as description:
+    long_description = description.read()
+
 setup(
     name=name,
     version=version,
@@ -32,7 +38,7 @@ setup(
     include_package_data=True,
     license="GPLv3",
     description="A parser for RIPE Atlas measurement results",
-    long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    long_description=long_description,
     url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
     download_url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
     author="Daniel Quinn",
