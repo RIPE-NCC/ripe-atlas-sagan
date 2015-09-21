ff15e8bcb450e0dce5a7d14a0ad3673c8de89140
Daniel Quinn <dquinn@ripe.net>
2015-09-21 18:38:01 +0200

    Converted to .rst and appended Changelog to the long_description

--- setup.py
+++ setup.py
@@ -20,42 +20,49 @@ if "SAGAN_WITHOUT_SSL" not in os.environ:
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
+changelog_path = os.path.join(os.path.dirname(__file__), "Changelog.rst")
+
 # Get the long description from README.md
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as description:
-    setup(
-        name="ripe.atlas.sagan",
-        version=__version__,
-        packages=["ripe", "ripe.atlas", "ripe.atlas.sagan"],
-        namespace_packages=["ripe", "ripe.atlas"],
-        include_package_data=True,
-        license="GPLv3",
-        description="A parser for RIPE Atlas measurement results",
-        long_description=description.read(),
-        url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
-        download_url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
-        author="Daniel Quinn",
-        author_email="dquinn@ripe.net",
-        maintainer="Daniel Quinn",
-        maintainer_email="dquinn@ripe.net",
-        install_requires=install_requires,
-        tests_require=tests_require,
-        extras_require={
-            "fast": ["ujson"],
-            "doc": ["sphinx"]
-        },
-        test_suite="nose.collector",
-        scripts=[
-            "scripts/parse_abuf"
-        ],
-        classifiers=[
-            "Operating System :: POSIX",
-            "Operating System :: Unix",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2.6",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.1",
-            "Programming Language :: Python :: 3.2",
-            "Programming Language :: Python :: 3.3",
-            "Topic :: Internet :: WWW/HTTP",
-        ],
-    )
+with open(readme_path) as readme:
+    with open(changelog_path) as changelog:
+        setup(
+            name="ripe.atlas.sagan",
+            version=__version__,
+            packages=["ripe", "ripe.atlas", "ripe.atlas.sagan"],
+            namespace_packages=["ripe", "ripe.atlas"],
+            include_package_data=True,
+            license="GPLv3",
+            description="A parser for RIPE Atlas measurement results",
+            long_description="{readme}\n\n{changelog}".format(
+                readme=readme.read(),
+                changelog=changelog.read()
+            ),
+            url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
+            download_url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
+            author="Daniel Quinn",
+            author_email="dquinn@ripe.net",
+            maintainer="Daniel Quinn",
+            maintainer_email="dquinn@ripe.net",
+            install_requires=install_requires,
+            tests_require=tests_require,
+            extras_require={
+                "fast": ["ujson"],
+                "doc": ["sphinx"]
+            },
+            test_suite="nose.collector",
+            scripts=[
+                "scripts/parse_abuf"
+            ],
+            classifiers=[
+                "Operating System :: POSIX",
+                "Operating System :: Unix",
+                "Programming Language :: Python",
+                "Programming Language :: Python :: 2.6",
+                "Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3.1",
+                "Programming Language :: Python :: 3.2",
+                "Programming Language :: Python :: 3.3",
+                "Topic :: Internet :: WWW/HTTP",
+            ],
+        )
