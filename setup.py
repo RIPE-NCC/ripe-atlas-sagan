7b28a8fa111f88f5b6442aeec2381770e13e3222
Robert Kisteleki <robertk@ripe.net>
2019-09-06 15:59:29 +0200

    drop py34, add py37

--- setup.py
+++ setup.py
@@ -54,11 +54,9 @@ with open(readme_path) as readme:
                 "Operating System :: Unix",
                 "Programming Language :: Python",
                 "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3.1",
-                "Programming Language :: Python :: 3.2",
-                "Programming Language :: Python :: 3.3",
-                "Programming Language :: Python :: 3.4",
                 "Programming Language :: Python :: 3.5",
+                "Programming Language :: Python :: 3.6",
+                "Programming Language :: Python :: 3.7",
                 "Topic :: Internet :: WWW/HTTP",
             ],
         )
