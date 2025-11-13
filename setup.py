0d57f8d8ab271de54fec4b05e6b5764f1f4b6b97
Francesco Iannuzzelli <fiannuzzelli@ripe.net>
2025-11-13 19:01:42 +0100

    Python 3.10-3.13

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-import os
+from os.path import abspath, dirname, join
+from os import environ
 from setuptools import setup
 
 __version__ = None
@@ -12,51 +13,45 @@ install_requires = [
 
 tests_require = ["nose"]
 
-if "SAGAN_WITHOUT_SSL" not in os.environ:
+if "SAGAN_WITHOUT_SSL" not in environ:
     install_requires.append("cryptography")
 
-# Allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+# Get proper long description for package
+current_dir = dirname(abspath(__file__))
+description = open(join(current_dir, "README.rst")).read()
+changes = open(join(current_dir, "CHANGES.rst")).read()
+long_description = "\n\n".join([description, changes])
 
-readme_path = os.path.join(os.path.dirname(__file__), "README.rst")
-changelog_path = os.path.join(os.path.dirname(__file__), "CHANGES.rst")
-
-# Get the long description from README.md
-with open(readme_path) as readme:
-    with open(changelog_path) as changelog:
-        setup(
-            name="ripe.atlas.sagan",
-            version=__version__,
-            packages=["ripe", "ripe.atlas", "ripe.atlas.sagan"],
-            namespace_packages=["ripe", "ripe.atlas"],
-            include_package_data=True,
-            license="GPLv3",
-            description="A parser for RIPE Atlas measurement results",
-            long_description="{readme}\n\n{changelog}".format(
-                readme=readme.read(),
-                changelog=changelog.read()
-            ),
-            url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
-            download_url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
-            author="The RIPE Atlas Team",
-            author_email="atlas@ripe.net",
-            maintainer="The RIPE Atlas Team",
-            maintainer_email="atlas@ripe.net",
-            install_requires=install_requires,
-            tests_require=tests_require,
-            extras_require={
-                "fast": ["ujson"],
-                "doc": ["sphinx"]
-            },
-            test_suite="nose.collector",
-            classifiers=[
-                "Operating System :: POSIX",
-                "Operating System :: Unix",
-                "Programming Language :: Python",
-                "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3.5",
-                "Programming Language :: Python :: 3.6",
-                "Programming Language :: Python :: 3.7",
-                "Topic :: Internet :: WWW/HTTP",
-            ],
-        )
+setup(
+    name="ripe.atlas.sagan",
+    version=__version__,
+    packages=["ripe", "ripe.atlas", "ripe.atlas.sagan"],
+    namespace_packages=["ripe", "ripe.atlas"],
+    include_package_data=True,
+    license="GPLv3",
+    description="A parser for RIPE Atlas measurement results",
+    long_description=long_description,
+    url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
+    download_url="https://github.com/RIPE-NCC/ripe.atlas.sagan",
+    author="The RIPE Atlas Team",
+    author_email="atlas@ripe.net",
+    maintainer="The RIPE Atlas Team",
+    maintainer_email="atlas@ripe.net",
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        "fast": ["ujson"],
+        "doc": ["sphinx"]
+    },
+    test_suite="nose.collector",
+    classifiers=[
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+)
